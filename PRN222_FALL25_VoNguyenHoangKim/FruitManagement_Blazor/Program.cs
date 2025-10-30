62659f112e2efc1de6a40f199e8df0a40216c277
bngbtran <tranbnb.2004@gmail.com>
2025-10-31 00:57:25 +0700

    finish UI Blazor

---
+++ PRN222_FALL25_VoNguyenHoangKim/FruitManagement_Blazor/Program.cs
@@ -0,0 +1,44 @@
+﻿using FruitManagement_Blazor.Components;
+using DAOs;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace FruitManagement_Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            // 🔹 Đăng ký DbContext
+            builder.Services.AddDbContext<FruitContext>(options =>
+                options.UseSqlServer("Server=localhost;Database=FruitDB;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+            // 🔹 Đăng ký Repository
+            builder.Services.AddScoped<IFruitRepository, FruitRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
